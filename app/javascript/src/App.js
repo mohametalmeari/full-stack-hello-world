bc2f763e849a53009d3cddd5b1336c6bf9291ede
mohametalmeari <mohamet.almeari@gmail.com>
2023-09-18 15:06:38 +0300

    Create Greeting API

---
+++ app/javascript/src/App.js
@@ -0,0 +1,6 @@
+const App = () => (
+  <div>
+    Hi
+  </div>
+);
+export default App;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Message.create(content: "Hello, how are you?")
+Message.create(content: "Hi there! How can I help you today?")
+Message.create(content: "Good morning! Have a great day.")
+Message.create(content: "Hey, it's nice to see you!")
+Message.create(content: "Welcome! Feel free to ask any questions.")
